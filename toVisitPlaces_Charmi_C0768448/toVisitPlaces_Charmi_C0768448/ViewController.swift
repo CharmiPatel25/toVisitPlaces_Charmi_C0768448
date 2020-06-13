f6876a1d9a8b140c8584a44563eae8834968bd43
CharmiPatel25 <C0768448@mylambton.ca>
2020-06-13 12:03:58 -0700

    initial commit

---
+++ toVisitPlaces_Charmi_C0768448/toVisitPlaces_Charmi_C0768448/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toVisitPlaces_Charmi_C0768448
+//
+//  Created by user174608 on 6/13/20.
+//  Copyright © 2020 charmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
